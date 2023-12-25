3876ad0f91aa50cac68c122b7ca5ed9e47dd9096
nanati45 <99168441+nanati45@users.noreply.github.com>
2023-12-25 20:27:52 +0300

    Added solution for Two Sum

---
+++ python_track/two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    y=[i,j]
+                    return y
+        
