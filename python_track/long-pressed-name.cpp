6baef389235e2ba7cf78659d923639c6dda2b056
nanati45 <99168441+nanati45@users.noreply.github.com>
2024-01-16 16:29:28 +0300

    Added solution for Long Pressed Name

---
+++ python_track/long-pressed-name.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0,j = 0, n, m;
+        n = name.size();
+        m = typed.size();
+
+        if (m < n){
+            return false;
+        }
+        while(i < n or j < m){
+            int tcount = 0;
+            int ncount = 0;
+            if (name[i] == typed[j]){
+                char curr = name[i];
+                while(typed[j] == curr) tcount++,j++;
+                while(name[i] == curr) ncount++,i++;
+                // cout << i <<" "<< j << " "<<tcount <<" " << ncount<< endl;
+
+            }
+            else{
+                return false;
+            }
+            if (ncount > tcount){
+                return false;
+            }
+            // cout << i <<" "<< j << " "<<tcount <<" " << ncount;
+            
+
+        }
+
+        return true;
+    }
+};
