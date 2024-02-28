16049367de267aae1937e085a90dd1153a18cfd2
nanati45 <99168441+nanati45@users.noreply.github.com>
2024-02-28 22:36:11 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ python_track/leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        ans = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                ans += min(tickets[i],tickets[k])
+            else:
+                ans += min(tickets[i],tickets[k]-1)
+            
+        return ans         
+            
